378b098bd03cbfd9fb13c24cd73cffc4d7932a2a
TLAMHutto <hutto.keaton@yahoo.com>
2022-04-11 21:48:19 -0500

    radio

---
+++ index.js
@@ -0,0 +1,9 @@
+//function to toggle class=idtoggle when clicked
+function toggle(id) {
+    var e = document.getElementById(alkali);
+    if (e.style.display == 'block')
+        e.style.display = 'none';
+    else
+        e.style.display = 'block';
+}
+toggle();
